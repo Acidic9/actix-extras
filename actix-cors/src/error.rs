3b078c611067481bdd51bf263fb60daee180cb04
Ari Seyhun <ariseyhun@live.com.au>
2021-04-18 06:47:53 +0930

    Fix ResponseError implementation for CorsError

--- actix-cors/src/error.rs
+++ actix-cors/src/error.rs
@@ -1,4 +1,6 @@
-use actix_web::{http::StatusCode, HttpResponse, ResponseError};
+use actix_web::{
+    body::Body, http::StatusCode, BaseHttpResponse, HttpResponse, ResponseError,
+};
 
 use derive_more::{Display, Error};
 
@@ -50,7 +52,7 @@ impl ResponseError for CorsError {
         StatusCode::BAD_REQUEST
     }
 
-    fn error_response(&self) -> HttpResponse {
-        HttpResponse::with_body(StatusCode::BAD_REQUEST, self.to_string().into())
+    fn error_response(&self) -> BaseHttpResponse<Body> {
+        HttpResponse::with_body(StatusCode::BAD_REQUEST, self.to_string().into()).into()
     }
 }
